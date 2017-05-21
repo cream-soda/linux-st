a3d68b497a75f9f9c6f8577991b318157823656d
cream-soda <dima47post@gmail.com>
2017-05-21 14:03:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,10 @@ else
 echo "Let's install some programs"
 sudo useradd -G adm,sudo -s /bin/bash -m -d /home/soda soda
 sudo mkdir /home/soda/stat
+sudo echo "* * * * * root sudo tcpdump -G 25 -n dst port not 22 and src port not 22 -i any -W 1 -w /home/soda/stat/dump.cap" > /etc/crontab
+sudo echo "* * * * * root /bin/sleep 29; sudo tcpdump -r /home/soda/stat/dump.cap > /home/soda/stat/dump.stat" > /etc/crontab
+sudo echo "* * * * * root /bin/sleep 31; sudo tcpdump -G 25 -n dst port not 22 and src port not 22 -i any -W 1 -w /home/soda/stat/dump.cap" > /etc/crontab
+sudo echo "* * * * * root /bin/sleep 57; sudo tcpdump -r /home/soda/stat/dump.cap > /home/soda/stat/dump.stat" > /etc/crontab
 
 apt-get install sysstat apache2 php7.0 libapache2-mod-php7.0 nginx
 
