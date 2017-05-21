0680c1f7806c2ab73419e71eecaabb12660e04cc
cream-soda <dima47post@gmail.com>
2017-05-21 23:36:42 +0300

    Update table_tt1.sh

--- table_tt1.sh
+++ table_tt1.sh
@@ -4,9 +4,9 @@ num_tcp=$(cat /home/soda/stat/dump.stat | grep -E "(TCP|tcp)" | wc -l)
 num_udp=$(cat /home/soda/stat/dump.stat | grep -E "(UDP|udp)" | wc -l)
 num_icmp=$(cat /home/soda/stat/dump.stat | grep -E "(ICMP|icmp)" | wc -l)
 
-num_tcp=$(echo "$num_tcp * 3" | bc)
-num_udp=$(echo "$num_udp * 3" | bc)
-num_icmp=$(echo "$num_icmp * 3" | bc)
+#num_tcp=$(echo "$num_tcp * 3" | bc)
+#num_udp=$(echo "$num_udp * 3" | bc)
+#num_icmp=$(echo "$num_icmp * 3" | bc)
 
 echo "<p>"
 echo "</p>"
