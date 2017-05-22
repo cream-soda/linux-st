89b1cae42f4427273b68877fffb6a240aa61b06b
cream-soda <dima47post@gmail.com>
2017-05-23 00:16:44 +0300

    Update table_tt1.sh

--- table_tt1.sh
+++ table_tt1.sh
@@ -8,22 +8,47 @@ num_tcp=$(echo "$num_tcp * 3" | bc)
 num_udp=$(echo "$num_udp * 3" | bc)
 num_icmp=$(echo "$num_icmp * 3" | bc)
 
+len_tcp=0
+for i in seq($num_tcp)
+do
+num_word=$(cat /home/soda/stat/dump.stat | grep -E "(TCP|tcp)" |  awk "(NR==$i)" | wc -w)
+len_tcp=$(echo "$len_tcp + $(cat /home/soda/stat/dump.stat | grep -E "(TCP|tcp)" |  awk '(NR==$i)' | cut -d" " -f$num_word)" | bc)
+done
+
+len_udp=0
+for i in seq($num_udp)
+do
+num_word=$(cat /home/soda/stat/dump.stat | grep -E "(UDP|udp)" |  awk "(NR==$i)" | wc -w)
+len_udp=$(echo "$len_udp + $(cat /home/soda/stat/dump.stat | grep -E "(UDP|udp)" |  awk '(NR==$i)' | cut -d" " -f$num_word)" | bc)
+done
+
+len_icmp=0
+for i in seq($num_icmp)
+do
+num_word=$(cat /home/soda/stat/dump.stat | grep -E "(ICMP|icmp)" |  awk "(NR==$i)" | wc -w)
+len_icmp=$(echo "$len_icmp + $(cat /home/soda/stat/dump.stat | grep -E "(ICMP|icmp)" |  awk '(NR==$i)' | cut -d" " -f$num_word)" | bc)
+done
+
 echo "<p>"
 echo "</p>"
 echo '<table align="left" border="1">'
 echo "<tr>"
-echo '<td colspan="2">Top talkers</td>'
+echo '<td colspan="3">Top talkers</td>'
 echo "</tr>"
 
 echo "<tr>"
 echo "<td>Protocol</td>"
 echo "<td>Packets</td>"
+echo "<td>Bytes</td>"
 echo "</tr>"
 
 echo "<tr>"
 echo "<td>TCP</td>"
 echo "<td>"
 echo $num_tcp
+echo "</td>"
+echo "<td>"
+echo $len_tcp
 echo "</td>"
 echo "</tr>"
 
@@ -32,12 +57,18 @@ echo "<td>UDP</td>"
 echo "<td>"
 echo $num_udp
 echo "</td>"
+echo "<td>"
+echo $len_udp
+echo "</td>"
 echo "</tr>"
 
 echo "<tr>"
 echo "<td>ICMP</td>"
 echo "<td>"
 echo $num_icmp
+echo "</td>"
+echo "<td>"
+echo $len_icmp
 echo "</td>"
 echo "</tr>"
 
