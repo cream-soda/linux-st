2d7c5248ab56c9df77452152f23c2c419a1ff8eb
cream-soda <dima47post@gmail.com>
2017-05-24 15:49:28 +0300

    Update table_mem.sh

--- table_mem.sh
+++ table_mem.sh
@@ -3,9 +3,9 @@
 num_d=$(echo "$(df | grep -Ev '(tmpfs|udev)' | wc -l) - 1" | bc)
 
 color (){
-    if [[ $(echo "$1 > 70" | bc) -eq 1 ]]; then
+    if [[ $(echo "$1 < 30" | bc) -eq 1 ]]; then
          OUT="<span style='color: red;'>$1</span>"
-    elif [[ $(echo "$1 > 50" | bc) -eq 1 ]]; then
+    elif [[ $(echo "$1 < 50" | bc) -eq 1 ]]; then
          OUT="<span style='color: yellow;'>$1</span>"
     else
          OUT="<span style='color: green;'>$1</span>"
@@ -37,12 +37,14 @@ echo "<td>"
   m1=$(cat /home/soda/stat/mem.stat | tail -$i | head -n1 | awk '{print $2}')
   m2=$(echo ${m1//,/.})
   m3=$(echo $m2 | cut -c -$(echo "$(expr length $m2) - 1" | bc))
+  m3=$(echo "100 - $m3" | bc )
   color $m3
 echo "</td>"
 echo "<td>"
   m1=$(cat /home/soda/stat/in_mem.stat | tail -$i | head -n1 | awk '{print $2}')
   m2=$(echo ${m1//,/.})
   m3=$(echo $m2 | cut -c -$(echo "$(expr length $m2) - 1" | bc))
+  m3=$(echo "100 - $m3" | bc )
   color $m3
 echo "</td>"
 
