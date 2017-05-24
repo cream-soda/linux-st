51bf282bb6fcca3fe026ca2cc31853d2defd4672
cream-soda <dima47post@gmail.com>
2017-05-24 16:41:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,11 +19,13 @@ echo "* * * * * root /bin/sleep 20; sudo tcpdump -q -r /home/soda/stat/dump.cap 
 echo "* * * * * root /bin/sleep 30; sudo tcpdump -G 20 -n dst port not 22 and src port not 22 -i any -q -W 1 -w /home/soda/stat/dump.cap" >> /etc/crontab
 echo "* * * * * root /bin/sleep 50; sudo tcpdump -q -r /home/soda/stat/dump.cap > /home/soda/stat/dump.stat" >> /etc/crontab
 
+apt-get update
+apt-get -y install sysstat apache2 php7.0 libapache2-mod-php7.0 bc tcpdump ethtool elinks
+systemctl stop apache2
+
 mkdir /home/soda/rubbish
 
-apt-get install -y sysstat apache2 php7.0 libapache2-mod-php7.0 bc
-systemctl stop apache2
-apt-get install -y nginx
+apt-get -y nginx
 systemctl stop nginx
 
 cd /home/soda/rubbish
@@ -37,7 +39,7 @@ cd /home/soda/rubbish
 git clone https://github.com/cream-soda/linux-st.git
 cd linux-st
 mv .htaccess /var/www/html/.htaccess
-mv stat.sh /etc/init.d/stat.sh
+mv stat.sh /home/soda/stat.sh
 mv systeminfo.php /var/www/html/systeminfo.php
 mv table_cpu.sh /home/soda/table_cpu.sh
 mv table_ios.sh /home/soda/table_ios.sh
@@ -57,12 +59,10 @@ systemctl start apache2
 cd /home/soda/rubbish
 git clone https://github.com/cream-soda/inst.git
 cd inst
-mv stat_init.sh /etc/init.d/stat_init.sh
-sudo rc-update add stat_init.sh defaults
+cat stat_init.sh > /etc/rc.local
 ###
 
-/etc/init.d/stat.sh &
-
+/home/soda/stat.sh &
+rm -r /home/soda/rubbish
 
 fi
-
